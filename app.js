c66e00a97d716241d36c7d61579e12ef6c479132
cj <shahid.alam57@gmail.com>
2021-11-23 10:23:53 +0530

    playlist commit

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 let mainVid= document.querySelector('.main-vid video')
 let playlist = document.querySelectorAll('.playlist .vid')
 let title = document.querySelector('.title')
+
+playlist.forEach(vid=>{
+    vid.addEventListener('click',()=>{
+        // console.log(vid.classList)
+        playlist.forEach(vid=>vid.classList.remove('active'))
+        vid.classList.add('active')
+    })
+})
