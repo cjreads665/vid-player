d2c48ca037de5e8e70169be5eaf64d3db7bea7b3
cj <shahid.alam57@gmail.com>
2021-11-23 10:06:09 +0530

    playlist commit

---
+++ app.js
@@ -0,0 +1,4 @@
+//code for playlist styling
+let mainVid= document.querySelector('.main-vid video')
+let playlist = document.querySelectorAll('.playlist .vid')
+let title = document.querySelector('.title')
